ae46d195458bda3d4c77eb2138aa2f316bee3361
Ben Franzi <me@benfranzi.com>
2023-06-26 10:44:20 +1000

    init

---
+++ src/index.ts
@@ -0,0 +1,114 @@
+import * as Alexa from 'ask-sdk-core';
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Welcome to Arexa! Ask me if it\'s daytime!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard("I'm a card title", speechText)
+      .getResponse();
+  }
+};
+
+const AskDaytimeIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskDaytimeIntent';
+  },
+  handle(handlerInput) {
+    const currentHour = (new Date()).getHours();
+    const speechText =  (currentHour > 6 && currentHour < 18) ? 'It is daytime!' : 'It is nighttime!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('A title, wow', speechText)
+      .getResponse();
+  }
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask me if it\'s daytime, that is it!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Wowza, I\'m a card title', speechText)
+      .getResponse();
+  }
+};
+
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Catch ya later skater!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Catch ya later skater in the title!', speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    // Any clean-up logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    const reprompt = 'I don\'t understand my man, what are you talking about?';
+
+    return handlerInput.responseBuilder
+      .speak(reprompt)
+      .reprompt(reprompt)
+      .getResponse();
+  }
+};
+
+let skill;
+
+export async function handler(event, context) {
+  console.log(`REQUEST++++${JSON.stringify(event)}`);
+  if (!skill) {
+    skill = Alexa.SkillBuilders.custom()
+      .addRequestHandlers(
+        LaunchRequestHandler,
+        AskDaytimeIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+      )
+      .addErrorHandlers(ErrorHandler)
+      .create();
+  }
+
+  const response = await skill.invoke(event, context);
+  console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+  return response;
+}
