019a330a5b49a9cf601e5064dc92a6f67783d4ba
Daniel Waldow <daniel.waldow@.lexicondigital.com.au>
2023-06-26 17:22:37 +1000

    feat: add feeding intent stub + active pet check

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,49 @@ import { v4 as uuid } from 'uuid';
 import {
   DynamoDBDocumentClient,
   PutCommand,
+  QueryCommand,
 } from "@aws-sdk/lib-dynamodb";
+
 const client = new DynamoDBClient({ region: 'us-east-1' });
 const dynamo = DynamoDBDocumentClient.from(client);
-const tableName = 'office-pet';
+const PET_TABLE = 'office-pet';
+const INTERACTION_TABLE = 'office-pet-interaction';
+
+async function getPet(handlerInput) {
+  const userId = getUserId(handlerInput.requestEnvelope);
+
+  const { Items } = await dynamo.send(
+    new QueryCommand({
+      TableName: PET_TABLE,
+      IndexName: 'userId-index',
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: { ":userId": userId },
+      ScanIndexForward: false,
+    })
+  );
+
+  const [latest] = Items || [];
+
+  return latest;
+}
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
   },
-  handle(handlerInput) {
+  async handle(handlerInput) {
+    const pet = await getPet(handlerInput);
+
+    if (!pet) {
+      return handlerInput.responseBuilder
+        .speak('Welcome to Office Pet, adopt a pet to get started')
+        .reprompt('Welcome to Office Pet, adopt a pet to get started')
+        .getResponse();
+    }
+
     return handlerInput.responseBuilder
-      .speak('Welcome to Office Pet')
-      .reprompt('Welcome to Office Pet')
+      .speak(`Welcome to Office Pet, ask me how ${pet.name} is doing`)
+      .reprompt(`Welcome to Office Pet, ask me how ${pet.name} is doing`)
       .getResponse();
   }
 };
@@ -27,14 +57,24 @@ const CreatePetIntentHandler = {
       && getIntentName(handlerInput.requestEnvelope) === 'CreatePetIntent';
   },
   async handle(handlerInput) {
+
+    const pet = await getPet(handlerInput);
+
+    if (pet) {
+      return handlerInput.responseBuilder
+        .speak(`You've already got a pet, what's wrong with ${pet.name}`)
+        .reprompt(`You've already got a pet, what's wrong with ${pet.name}`)
+        .getResponse();
+    }
+
     const userId = getUserId(handlerInput.requestEnvelope);
     const petName = 'Bob';
 
     await dynamo.send(
       new PutCommand({
-        TableName: tableName,
+        TableName: PET_TABLE,
         Item: {
-          'office-pet-key': uuid(),
+          petId: uuid(),
           userId: userId,
           name: petName,
           adopted: new Date().toISOString(),
@@ -46,6 +86,48 @@ const CreatePetIntentHandler = {
         .speak(`Congratulations! You have successfully adopted your new pet. Their name is ${petName}. Say hi to ${petName}`)
         .reprompt(`Say hi to ${petName}`)
         .getResponse();
+  }
+}
+
+enum InteractionType {
+  FEED = 'FEED',
+  CLEAN = 'CLEAN',
+}
+
+const FeedPetIntentHandler = {
+  canHandle(handlerInput) {
+    return getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && getIntentName(handlerInput.requestEnvelope) === 'FeedPetIntent';
+
+     // TODO: prevent when no pet selected
+  },
+  async handle(handlerInput) {
+    const pet = await getPet(handlerInput);
+
+    if (!pet) {
+      return handlerInput.responseBuilder
+        .speak(`You don't have a pet, go adopt one`)
+        .reprompt(`You don't have a pet, go adopt one`)
+        .getResponse();
+    }
+
+    const { petId } = pet;
+
+    await dynamo.send(
+      new PutCommand({
+        TableName: INTERACTION_TABLE,
+        Item: {
+          petId,
+          interactionType: InteractionType.FEED,
+          date: new Date().toISOString()
+        },
+      })
+    );
+
+    return handlerInput.responseBuilder
+      .speak(``)
+      .reprompt(``)
+      .getResponse();
   }
 }
 
