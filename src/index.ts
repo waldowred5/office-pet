53efe302d78210a97d3ad452f1a80f72dcddc729
Ben Franzi <me@benfranzi.com>
2023-06-26 11:28:56 +1000

    chore: update welcome text

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as Alexa from 'ask-sdk-core';
+import { getRequestType, getIntentName, SkillBuilders } from 'ask-sdk-core';
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    return getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechText = 'Welcome to Office Pet! Ask me if it\'s daytime!';
+    const speechText = 'Bob is dead!';
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -16,15 +16,15 @@ const LaunchRequestHandler = {
 };
 
 const AskDaytimeIntentHandler = {
-  canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskDaytimeIntent';
-  },
-  handle(handlerInput) {
-    const currentHour = (new Date()).getHours();
-    const speechText =  (currentHour > 6 && currentHour < 18) ? 'It is daytime!' : 'It is nighttime!';
+canHandle(handlerInput) {
+  return getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+    && getIntentName(handlerInput.requestEnvelope) === 'AskDaytimeIntent';
+},
+handle(handlerInput) {
+  const currentHour = (new Date()).getHours();
+  const speechText =  (currentHour > 6 && currentHour < 18) ? 'It is daytime!' : 'It is nighttime!';
 
-    return handlerInput.responseBuilder
+  return handlerInput.responseBuilder
       .speak(speechText)
       .withSimpleCard('A title, wow', speechText)
       .getResponse();
@@ -33,8 +33,8 @@ const AskDaytimeIntentHandler = {
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    return getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
     const speechText = 'You can ask me if it\'s daytime, that is it!';
@@ -50,9 +50,9 @@ const HelpIntentHandler = {
 
 const CancelAndStopIntentHandler = {
   canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    return getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
     const speechText = 'Catch ya later skater!';
@@ -67,7 +67,7 @@ const CancelAndStopIntentHandler = {
 
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    return getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
   },
   handle(handlerInput) {
     // Any clean-up logic goes here.
@@ -95,7 +95,7 @@ let skill;
 export async function handler(event, context) {
   console.log(`REQUEST++++${JSON.stringify(event)}`);
   if (!skill) {
-    skill = Alexa.SkillBuilders.custom()
+    skill = SkillBuilders.custom()
       .addRequestHandlers(
         LaunchRequestHandler,
         AskDaytimeIntentHandler,
